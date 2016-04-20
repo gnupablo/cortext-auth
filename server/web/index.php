8897ccc2a66690a6c28ea0ad9a87d0ce51be76fc
Guillaume Orsal <guillaume@orsal.net>
2016-04-20 18:20:12 +0200

    Ajout des Terms of use

--- server/web/index.php
+++ server/web/index.php
@@ -13,5 +13,10 @@ $app = require_once dirname(__DIR__).'/app/bootstrap.php';
 // create an http foundation request implementing OAuth2_RequestInterface
 $request = OAuth2\HttpFoundationBridge\Request::createFromGlobals();
 
+// Integration des Terms of use
+$app->get('/terms', function () use ($app) {
+    return $app['twig']->render('terms.twig', array('layout_template' => '@user/layout.twig'));
+})->bind('terms');
+
 //let's rock !
 $app->run($request);
