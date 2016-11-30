616f17cac3455d0120743adbb9409bcbc659323c
Philippe Breucker <breucker@ifris.org>
2016-11-30 11:29:24 +0100

    adding authorization field to db

--- server/data/rebuild_db.php
+++ server/data/rebuild_db.php
@@ -35,8 +35,7 @@ if($params['driver']=='pdo_mysql'){
     $db->exec('CREATE TABLE oauth_authorization_codes (authorization_code TEXT, client_id TEXT, user_id TEXT, redirect_uri TEXT, expires TIMESTAMP, scope TEXT)');
     $db->exec('CREATE TABLE oauth_refresh_tokens (refresh_token TEXT, client_id TEXT, user_id TEXT, expires TIMESTAMP, scope TEXT)');
     $db->exec("CREATE TABLE `users` (`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,`email` VARCHAR(100) NOT NULL DEFAULT '',`password` VARCHAR(255) NOT NULL DEFAULT '',`salt` VARCHAR(255) NOT NULL DEFAULT '',`roles` VARCHAR(255) NOT NULL DEFAULT '',`name` VARCHAR(100) NOT NULL DEFAULT '',`time_created` INT NOT NULL DEFAULT 0,PRIMARY KEY (`id`),UNIQUE KEY `unique_email` (`email`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
-    $db->exec("CREATE TABLE `users_infos` ( `user_id` int(11) UNSIGNED NOT NULL, `description` TEXT DEFAULT NULL, `website` VARCHAR(255) DEFAULT NULL, `birthdate` date DEFAULT NULL, `last_connexion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `city` VARCHAR(250) DEFAULT NULL, `country` VARCHAR(250) DEFAULT NULL, `institution` VARCHAR(500) DEFAULT NULL, `activity_domain` VARCHAR(500) DEFAULT NULL, `research_domain` VARCHAR(500) DEFAULT NULL, PRIMARY KEY (`user_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
-
+    $db->exec("CREATE TABLE `users_infos` (   `user_id` int(11) unsigned NOT NULL,   `description` text,   `website` varchar(255) DEFAULT NULL,   `birthdate` date DEFAULT NULL,   `last_connexion` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,   `city` varchar(250) DEFAULT NULL,   `country` varchar(250) DEFAULT NULL,   `institution` varchar(500) DEFAULT NULL,   `activity_domain` varchar(500) DEFAULT NULL,   `research_domain` varchar(500) DEFAULT NULL,   `authorizations` text NOT NULL COMMENT 'json formatted list of authorization, for exemple URI that the user can access' ) ENGINE=InnoDB DEFAULT CHARSET=utf8;");
   }
   catch (PDOException $e){
     echo "PDO creation Error: ".$e->getMessage(); 
